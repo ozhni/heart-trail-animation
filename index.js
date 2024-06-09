1050047b635b428703e8091502abad975ec006ee
ozhni <mahsaqholami@gmail.com>
2024-06-09 19:41:41 +0330

    animate & colorate hearts

--- index.js
+++ index.js
@@ -9,5 +9,12 @@ bodyEl.addEventListener("mousemove", (event) => {
   spanEl.style.left = xPos + "px";
   spanEl.style.top = yPos + "px";
 
+  const size = Math.random() * 100;
+  spanEl.style.width = size + "px";
+  spanEl.style.height = size + "px";
+
   bodyEl.appendChild(spanEl);
+  setTimeout(() => {
+    spanEl.remove();
+  }, 3000);
 });
