096648a01b890e60629a97dc4fcc46df887c3917
ozhni <mahsaqholami@gmail.com>
2024-06-09 19:03:11 +0330

    make heart by mousemove

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+const bodyEl = document.querySelector("body");
+const heartElm = document.querySelector("span");
+
+bodyEl.addEventListener("mousemove", (event) => {
+  const xPos = event.offsetX;
+  const yPos = event.offsetY;
+  const spanEl = document.createElement("span");
+  spanEl.style.left = xPos + "px";
+  spanEl.style.top = yPos + "px";
+
+  bodyEl.appendChild(spanEl);
+});
